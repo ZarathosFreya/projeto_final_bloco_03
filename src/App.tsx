2749bdc7a1f4d24029d461239fa9243bc7db4d88
ZarathosFreya <dessa.neves99@gmail.com>
2025-02-25 11:52:09 -0300

    :sparkles: Implementação das Rotas

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,9 @@ import Navbar from './components/navbar/Navbar'
 import Home from './pages/home/Home'
 import './App.css'
 import Footer from './components/footer/Footer'
+import DeletarCategoria from './components/categorias/deletarcategoria/DeletarCategoria'
+import FormCategoria from './components/categorias/formcategoria/FormCategoria'
+import ListaCategoria from './components/categorias/listacategoria/ListaCategoris'
 
 function App() {
   return (
@@ -13,6 +16,10 @@ function App() {
             <Routes>
               <Route path="/" element={<Home />} />
               <Route path="/home" element={<Home />} />
+              <Route path="/categorias" element={<ListaCategoria />} />
+              <Route path="/cadastrarcategoria" element={<FormCategoria />} />
+              <Route path="/editarcategoria/:id" element={<FormCategoria />} />
+              <Route path="/deletarcategoria/:id" element={<DeletarCategoria />} />
             </Routes>
           </div>
           <Footer />
